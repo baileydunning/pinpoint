39ef039769d0a1f366ff5031b0c9bcb425b0a3eb
bailey <bailey@harperdb.io>
2026-05-21 16:03:57 -0600

    clean up

--- resources.js
+++ resources.js
@@ -2,28 +2,9 @@ import { tables } from "harper";
 
 const HighScoresTable = tables.HighScores
 
-export class HighScores extends HighScoresTable {
+export class high_scores extends HighScoresTable {
     static loadAsInstance = false;
     allowRead() {
-		return true
+	    return true
 	}
-
-    async get() {
-        const results = await HighScoresTable.get();
-        return {
-            statusCode: 200,
-            body: results,
-        }
-    }
-
-    async post(target, data) {
-
-        const record = { ...data, createdAt: new Date().toISOString() };
-        
-        await HighScoresTable.create(record);
-        return {
-            statusCode: 201,
-            body: record,
-        }
-    }
 }
