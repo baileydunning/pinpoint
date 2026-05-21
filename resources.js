3a0d6ae4921bc8125a3c8ba158bccb2f0da4a436
bailey <bailey@harperdb.io>
2026-05-21 16:18:54 -0600

    try rm this

--- resources.js
+++ resources.js
@@ -3,7 +3,6 @@ import { tables } from "harper";
 const HighScoresTable = tables.HighScores
 
 export class high_scores extends HighScoresTable {
-    static loadAsInstance = false;
     allowRead() {
 	    return true
 	}
